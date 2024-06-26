6abb718fc6f1ab8e9eaf8d6ed665078d605648cd
Adebola <adebola@poptin.com>
2024-06-26 18:45:56 +0100

    added image assets, install fonts, OnboardingScreen design

--- screens/onboarding/OnBoardingScreen.tsx
+++ screens/onboarding/OnBoardingScreen.tsx
@@ -1,10 +1,68 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
+// import { useFonts } from "expo-font";
+import { useFonts, Raleway_700Bold } from "@expo-google-fonts/raleway";
+import { Nunito_400Regular, Nunito_700Bold } from "@expo-google-fonts/nunito";
+import { LinearGradient } from "expo-linear-gradient";
+import { styles } from "@/styles/onboarding/onboarding";
 
 export default function OnBoardingScreen() {
+  let [fontsLoaded, fontError] = useFonts({
+    Raleway_700Bold,
+    Nunito_400Regular,
+    Nunito_700Bold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>OnBoardingScreen</Text>
-    </View>
+    <LinearGradient
+      colors={["#E5ECF9", "#F6F7F9"]}
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <View style={styles.firstContainer}>
+        <View>
+          <Image source={require("@/assets/logo.png")} style={styles.logo} />
+          <Image source={require("@/assets/onboarding/shape_9.png")} />
+        </View>
+        <View style={styles.titleWrapper}>
+          <Image
+            source={require("@/assets/onboarding/shape_3.png")}
+            style={styles.titleTextShape1}
+          />
+          <Text style={[styles.titleText, { fontFamily: "Raleway_700Bold" }]}>
+            Start Learning With
+          </Text>
+          <Image
+            style={styles.titleTextShape2}
+            source={require("@/assets/onboarding/shape_2.png")}
+          />
+        </View>
+        <View>
+          <Image
+            source={require("@/assets/onboarding/shape_6.png")}
+            style={styles.titleTextShape3}
+          />
+          <Text style={[styles.titleText, { fontFamily: "Raleway_700Bold" }]}>
+            TechBola
+          </Text>
+        </View>
+        <View style={styles.dscpWrapper}>
+          <Text style={[styles.dscpText, { fontFamily: "Nunito_400Regular" }]}>
+            Explore a variety of interactive lesson,
+          </Text>
+          <Text style={[styles.dscpText, { fontFamily: "Nunito_400Regular" }]}>
+            video, quizze & assignment.
+          </Text>
+        </View>
+        <TouchableOpacity style={styles.buttonWrapper}>
+          <Text style={[styles.buttonText, { fontFamily: "Nunito_700Bold" }]}>
+            Getting Started
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
 }
